e79d28388f253b600fe74fada37afaf95c9ba2f6
Arkhesiae <nicolas.m.gardet@gmail.com>
2025-08-10 11:40:19 +0200

    Modification de la configuration de Vite pour changer la base de l'application de './' à '/'.

--- front/vite.config.js
+++ front/vite.config.js
@@ -12,7 +12,7 @@ import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     VueRouter(),
     Layouts(),

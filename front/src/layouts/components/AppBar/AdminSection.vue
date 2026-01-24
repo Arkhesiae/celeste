f503e31ee6f3867a5b7f011960cd08b653cf7918
Nico <nicolas.m.gardet@gmail.com>
2026-01-24 20:27:38 +0100

    fix: admin section

--- front/src/layouts/components/AppBar/AdminSection.vue
+++ front/src/layouts/components/AppBar/AdminSection.vue
@@ -64,40 +64,7 @@
         <v-icon size="small">mdi-account-question-outline</v-icon>
       </v-badge>
     </v-btn>
-    <!-- Lien vers les tickets pour admin master -->
-    <v-btn v-if="isAdmin" icon @click="$emit('navigate-email')">
-
-      <v-icon size="small">mdi-email-outline</v-icon>
-
-      <!-- Lien vers les tickets pour admin master -->
-      <v-btn
-        v-if="isAdmin"
-        icon
-        @click="$emit('navigate-tickets')"
-      >
-        <v-badge
-          rounded="lg"
-          offset-x="10"
-          offset-y="28"
-          :content="messageCount"
-          :model-value="messageCount > 0"
-          color="pending"
-        >
-          <v-icon size="small">
-            mdi-account-question-outline
-          </v-icon>
-        </v-badge>
-      </v-btn>
-      <!-- Lien vers les tickets pour admin master -->
-      <v-btn
-        v-if="isAdmin"
-        icon
-        @click="$emit('navigate-email')"
-      >
-        <v-icon size="small">
-          mdi-email-outline
-        </v-icon>
-      </v-btn>
+   
     </div>
   </div>
 </template>
